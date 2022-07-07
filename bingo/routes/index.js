ab7445d27adfb26f06e23c31623ba430e3e2506f
Carlos Andres Valencia <anvaloso1@gmail.com>
2022-07-07 09:58:52 -0500

    Documentación.
    Se documenta parte del código.

--- bingo/routes/index.js
+++ bingo/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * Ruta index.
+ */
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Login' });
--- bingo/routes/users.js
+++ bingo/routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * Ruta Users.
+ */
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('signup');
